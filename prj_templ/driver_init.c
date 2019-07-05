3d6169838998004d7980c725b298c7339ae78f25
Michael Welsch <michael.welsch@hsu-hh.de>
2019-07-05 15:50:36 +0200

    Add files via upload

---
+++ prj_templ/driver_init.c
@@ -0,0 +1,31 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PA17
+
+	gpio_set_pin_level(PA17,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA17, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA17, GPIO_PIN_FUNCTION_OFF);
+}
