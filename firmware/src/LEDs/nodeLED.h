347184eda3f502844318a93dbb934ac9294f3828
Isabell <isabell.bruns@hsu-hh.de>
2019-11-06 16:55:21 +0100

    Changed LED order.

--- firmware/src/LEDs/nodeLED.h
+++ firmware/src/LEDs/nodeLED.h
@@ -39,7 +39,10 @@ protected:
     int m_CurBlinkingPeriod=0;
     int m_BlinkingPeriodLimit=0;
 
-    inline uint16_t get_zerob_ind(){ return static_cast<uint16_t>(m_nLED)-1; }
+    inline uint16_t get_zerob_ind(){ return (5 + ((static_cast<uint16_t>(m_nLED))*(-1)))-1; }
+ //   inline uint16_t get_zerob_ind(){ return static_cast<uint16_t>(5 + ((m_nLED)*(-1)))-1; }
+//    inline uint16_t get_zerob_ind(){ return static_cast<uint16_t>(m_nLED)-1; }
+
 
 public:
     CLED(typeLED nLED);
