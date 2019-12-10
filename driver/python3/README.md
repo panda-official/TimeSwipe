18bb359e9b2b4c70b30a0aabb93de5cf7eea6458
Ilya Gavrilov <gilyav@gmail.com>
2019-12-10 17:10:41 +0300

    libtimeswipe: atomic lib dependency; python wrapper: install_requires;  Readme.md update

--- driver/python3/README.md
+++ driver/python3/README.md
@@ -13,6 +13,8 @@ python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 ## Install on target
 
 ```
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade cmake_setuptools
 sudo pip3 install -i https://test.pypi.org/simple/ --upgrade timeswipe
 ```
 
--- driver/python3/setup.py
+++ driver/python3/setup.py
@@ -2,7 +2,7 @@ import setuptools
 from cmake_setuptools import *
 setuptools.setup(
      name='timeswipe',
-     version='0.1',
+     version='0.2',
      ext_modules=[CMakeExtension('timeswipe_py')],
      cmdclass={'build_ext': CMakeBuildExt},
      author="Ilya Gavrilov",
@@ -12,6 +12,7 @@ setuptools.setup(
      long_description_content_type="text/markdown",
      url="https://github.com/panda-official/TimeSwipe",
      packages=setuptools.find_packages(),
+     install_requires = ['cmake_setuptools'],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
