d036b37a2c40e4ae370a70ab65442cdd6bdcc424
Isabell <isabell.bruns@hsu-hh.de>
2020-01-08 16:51:00 +0100

    Revert "Merge branch 'master' of https://github.com/panda-official/TimeSwipe"
    This reverts commit 0f4359efb71385130bbacb708074de4bfdadc3ec, reversing
    changes made to 7526bc9121b041e7368f7394e3e86b0d2da2daf8.

---
+++ firmware/src/JSONstuff/json_base.cpp
@@ -0,0 +1,10 @@
+/*
+This Source Code Form is subject to the terms of the GNU General Public License v3.0.
+If a copy of the GPL was not distributed with this
+file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
+Copyright (c) 2019 Panda Team
+*/
+
+#include "json_base.h"
+
+int CJSONbase::m_nLockCmdSubsysCnt=0;
