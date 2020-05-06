3b079f38c6c82e8524bfdb88f96446477dd39641
vnk <vnk@vnkPC.home>
2020-05-06 13:49:41 +0300

    Revert "[+] v0.0.11"
    This reverts commit 4d6e09387b50b5c3d3fa446c34a79e0b2e140a43.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -63,7 +63,7 @@ int sys_clock_init(void);
 
 int main(void)
 {
-        auto pVersion=std::make_shared<CSemVer>(0,0,11);
+        auto pVersion=std::make_shared<CSemVer>(0,0,10);
 
         CSamNVMCTRL::Instance(); //check/setup SmartEEPROM before clock init
 
