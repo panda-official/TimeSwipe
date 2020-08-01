e5bec95f5067a8eeebdfc2d54dc5ec916848e36c
vnk <vnk@vnkPC.home>
2020-08-01 21:26:36 +0300

    [+] updated to v0.0.15

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,7 +2,7 @@
 This Source Code Form is subject to the terms of the GNU General Public License v3.0.
 If a copy of the GPL was not distributed with this
 file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
-Copyright (c) 2019 Panda Team
+Copyright (c) 2019-2020 Panda Team
 */
 
 //build for ADCs-DACs:
