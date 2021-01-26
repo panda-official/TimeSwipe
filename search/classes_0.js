3fff1cfce5005ba8e54020f4f14602a7afa2d194
dmitigr <dmitigr@users.noreply.github.com>
2021-01-26 12:00:32 +0000

    deploy: a4b431b42111c9e530148242e14a961c984e563a

--- search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['bridge_43',['Bridge',['../classTimeSwipeEvent_1_1Bridge.html',1,'TimeSwipeEvent']]],
-  ['button_44',['Button',['../classTimeSwipeEvent_1_1Button.html',1,'TimeSwipeEvent']]]
-];
