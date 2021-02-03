57fed6390bb6da4b0367309220cab597b7a261fa
Dmitry Igrishin <dmitigr@gmail.com>
2021-02-03 18:34:10 +0300

    More robust CMake stuff
    It resolves #118.

--- cmake/arm.cmake
+++ cmake/arm.cmake
@@ -15,41 +15,40 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 if(PANDA_BUILD_ARM)
-  set(sys_proc "armv7l")
+  if(PANDA_BUILD_ARM64)
+    set(default_sys_proc "aarch64")
+  else()
+    set(default_sys_proc "armv7l")
+  endif()
+
   if(PANDA_BUILD_ARM_BARE_METAL)
     set(default_sys_name "Generic")
-    set(default_c "arm-none-eabi-gcc")
-    set(default_cpp "arm-none-eabi-g++")
+    if(PANDA_BUILD_ARM64)
+      set(default_c "aarch64-none-eabi-gcc")
+      set(default_cpp "aarch64-none-eabi-g++")
+    else()
+      set(default_c "arm-none-eabi-gcc")
+      set(default_cpp "arm-none-eabi-g++")
+    endif()
 
     # Retarget to avoid linkage issues upon compiler testing.
     set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs")
     set(CMAKE_EXE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS}")
   else()
     set(default_sys_name "Linux")
-
     if(PANDA_BUILD_ARM64)
-      set(sys_proc "aarch64")
       set(default_c "aarch64-linux-gnu-gcc")
       set(default_cpp "aarch64-linux-gnu-g++")
     else()
       set(default_c "arm-linux-gnueabihf-gcc")
       set(default_cpp "arm-linux-gnueabihf-g++")
     endif()
-
   endif()
 
   if(WIN32)
     foreach(tool default_c default_cpp)
       set(${tool} "${${tool}}.exe")
     endforeach()
-  endif()
-
-  if(NOT DEFINED CMAKE_SYSTEM_NAME)
-    # CMake automatically set CMAKE_CROSSCOMPILING to True after this.
-    set(CMAKE_SYSTEM_NAME ${default_sys_name})
-  endif()
-  if(NOT DEFINED CMAKE_SYSTEM_PROCESSOR)
-    set(CMAKE_SYSTEM_PROCESSOR ${default_sys_proc})
   endif()
 
   if(NOT DEFINED CMAKE_C_COMPILER)
@@ -63,6 +62,19 @@ if(PANDA_BUILD_ARM)
   # where the cross-compiled stuff like Boost is installed.
   if(NOT DEFINED CMAKE_PREFIX_PATH)
     list(INSERT CMAKE_PREFIX_PATH 0 "/opt/arm")
+  endif()
+
+  if(NOT DEFINED CMAKE_SYSTEM_PROCESSOR)
+    set(CMAKE_SYSTEM_PROCESSOR ${default_sys_proc})
+  endif()
+
+  if(NOT CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL CMAKE_SYSTEM_PROCESSOR)
+    if(NOT DEFINED CMAKE_SYSTEM_NAME)
+      # CMake automatically set CMAKE_CROSSCOMPILING to True if CMAKE_SYSTEM_NAME
+      # was set manually. Note, that the effect of implicit modification of
+      # CMAKE_CROSSCOMPILING is visible only after the project() CMake command.
+      set(CMAKE_SYSTEM_NAME ${default_sys_name})
+    endif()
   endif()
 endif()
 
