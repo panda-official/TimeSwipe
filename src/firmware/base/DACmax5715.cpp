568bc111d9e35ab41e609d2ac69cd8de888781b9
Dmitry Igrishin <dmitigr@gmail.com>
2021-03-22 17:44:56 +0300

    A bit safer code

--- src/firmware/base/DACmax5715.cpp
+++ src/firmware/base/DACmax5715.cpp
@@ -11,13 +11,13 @@ Copyright (c) 2019 Panda Team
 
 CDac5715sa::CDac5715sa(CSPI *pBus, std::shared_ptr<IPin> pCS, typeDac5715chan nChan, float RangeMin, float RangeMax)
 {
-	m_pBus=pBus;
+    m_pBus=pBus;
     m_pCS=pCS;
-	m_chan=nChan;
+    m_chan=nChan;
 
     //! setup the raw-binary range and user defined user range
     m_IntRange=4095;
-	SetRange(RangeMin, RangeMax);
+    SetRange(RangeMin, RangeMax);
 }
 
 //driver function:
@@ -27,29 +27,38 @@ void CDac5715sa::DriverSetVal(float val, int out_bin)
      * m_pBus->set_phpol(false, true);
      * setup phase & polarity: phase=0(not shifted), polarity=true(1, bus idle state=HIGH)
      */
-	m_pBus->set_phpol(false, true);
+    m_pBus->set_phpol(false, true);
 
     //! m_pBus->set_tprofile_divs(0xff, 0, 0xff);
     //! setup the bus timing profile:
     //! ---minimal time to HOLD CS HIGH---___delay in between transfers___---delay before SCK is continued
-	m_pBus->set_tprofile_divs(0xff, 0, 0xff);
+    m_pBus->set_tprofile_divs(0xff, 0, 0xff);
 
     //! m_pBus->set_baud_div(0xff);
     //! setup the bus buadrate divisor: rate=clock_speed/255;
-	m_pBus->set_baud_div(0xff);
+    m_pBus->set_baud_div(0xff);
 
     //! cmd<<(0x30+(int)m_chan)<<((out_bin>>4)&0xff)<<((out_bin<<4)&0xff);
     //! forms a controlling message to be sent via SPI(MAX5715 manual, page 18)
     //! 1st byte: command 3(code value and load value "CODEn_LOADn")+channel number
     //! 2nd byte: control word high-byte
     //! 3nd byte: control word low byte
-	CFIFO cmd;
-	cmd<<(0x30+(int)m_chan)<<((out_bin>>4)&0xff)<<((out_bin<<4)&0xff);
+    const unsigned char command = 0x30 + static_cast<int>(m_chan);
+    // We need only 12 bits which must be left-shifted.
+    const unsigned int u_out_bin = out_bin;
+    static_assert(sizeof(u_out_bin) >= 2);
+    const unsigned char data8 = (u_out_bin >> 4) & 0xff; // left 8 bits
+    const unsigned char data4 = (u_out_bin << 4) & 0xff; // right 4 bits
+
+    CFIFO cmd;
+    cmd += command;
+    cmd += data8;
+    cmd += data4;
 
     m_pCS->Set(true);
     //os::uwait(80);
 
-	m_pBus->send(cmd);
+    m_pBus->send(cmd);
 
     m_pCS->Set(false);
     //os::uwait(80);
