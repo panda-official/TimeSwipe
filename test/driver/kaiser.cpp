e42e3ce949198cff9533a8a02bf652e1c2aa2a8f
Dmitry Igrishin <dmitigr@gmail.com>
2021-03-31 18:13:06 +0300

    Make test parallel

--- test/driver/kaiser.cpp
+++ test/driver/kaiser.cpp
@@ -9,11 +9,28 @@
 
 #include "resampler.hpp"
 
+#include <thread>
+#include <vector>
+
 int main()
 {
-  constexpr unsigned max_down_factor{500};
-  for (unsigned down{1}; down <= max_down_factor; ++down) {
-    TimeSwipeResamplerOptions opts{1, down};
-    TimeSwipeResampler resampler{std::move(opts)};
+  const unsigned thread_count{std::thread::hardware_concurrency()};
+  std::vector<std::thread> threads(thread_count);
+  constexpr unsigned max_factor{500};
+  const auto rest{max_factor % thread_count};
+  const auto step{max_factor / thread_count};
+  for (unsigned i{}; i < thread_count; ++i) {
+    const auto r{(i == thread_count - 1) ? rest : 0};
+    threads[i] = std::thread{[up_from = step*i + 1, up_to = step * (i + 1) + r]
+    {
+      for (unsigned up{up_from}; up <= up_to; ++up) {
+        for (unsigned down{1}; down <= max_factor; ++down) {
+          TimeSwipeResamplerOptions opts{up, down};
+          TimeSwipeResampler resampler{std::move(opts)};
+        }
+      }
+    }};
   }
+  for (auto& t : threads)
+    t.join();
 }
