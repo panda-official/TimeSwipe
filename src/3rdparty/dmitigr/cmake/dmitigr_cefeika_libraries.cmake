3a5ba03a4d9ce571c42ba15f7b67c108bd403782
Dmitry Igrishin <dmitigr@gmail.com>
2021-05-28 17:02:49 +0300

    Change the way of consuming Cefeika

--- src/3rdparty/dmitigr/cmake/dmitigr_cefeika_libraries.cmake
+++
@@ -1,5 +0,0 @@
-# -*- cmake -*-
-# Copyright (C) Dmitry Igrishin
-# For conditions of distribution and use, see file LICENSE.txt
-
-set(dmitigr_cefeika_libraries "assert;filesystem;math;progpar;str")
--- src/3rdparty/dmitigr/tool/cefeika_deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (C) Dmitry Igrishin
-
-cefeika_libraries="assert filesystem math progpar str"
-assert_deps=""
-filesystem_deps=""
-math_deps="assert"
-progpar_deps="assert filesystem"
-str_deps="assert"
