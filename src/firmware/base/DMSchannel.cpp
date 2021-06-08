ff3bcd6a9ef2f160d8818389f5806f5fa4ede2f1
Dmitry Igrishin <dmitigr@gmail.com>
2021-06-09 01:16:31 +0300

    Refactoring

--- src/firmware/base/DMSchannel.cpp
+++ src/firmware/base/DMSchannel.cpp
@@ -5,53 +5,22 @@ file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
 Copyright (c) 2019-2020 Panda Team
 */
 
+#include "../../common/gain.hpp"
 #include "../../common/HatsMemMan.h"
 #include "base/DMSchannel.h"
 #include "control/nodeControl.h"
 
+/// FIXME: namespace
+using namespace panda::timeswipe;
+
 void CDMSchannel::SetAmpGain(float GainValue)
 {
-    constexpr float ogain_factor{1.375};
-    constexpr float GainTab[]={
-        1,
-        1*ogain_factor,
-        2,
-        2*ogain_factor,
-        4,
-        4*ogain_factor,
-        8,
-        8*ogain_factor,
-        16,
-        16*ogain_factor,
-        32,
-        32*ogain_factor,
-        64,
-        64*ogain_factor,
-        128,
-        128*ogain_factor,
-        256,
-        256*ogain_factor,
-        512,
-        512*ogain_factor,
-        1024,
-        1024*ogain_factor
-    };
-    constexpr size_t tsize{sizeof(GainTab)/sizeof(float)};
-    static_assert(!(tsize % 2));
-    size_t el{};
-    for(; el<tsize; el++)
-    {
-        if(GainValue<GainTab[el])
-            break;
-    }
-    if(el>0)
-        el--;
-
-
-    if(m_pPGA->SetGains( static_cast<CPGA280::igain>(el/2), static_cast<CPGA280::ogain>(el%2) ))
-    {
-        m_nGainIndex=el;
-        m_ActualAmpGain=GainTab[el];
+    const auto index = OgainTableIndex(GainValue);
+    const auto igain = static_cast<CPGA280::igain>(index / 2);
+    const auto ogain = static_cast<CPGA280::ogain>(index % 2);
+    if (m_pPGA->SetGains(igain, ogain)) {
+        m_nGainIndex = index;
+        m_ActualAmpGain = ogain_table[index];
         UpdateOffsets();
     }
 }
