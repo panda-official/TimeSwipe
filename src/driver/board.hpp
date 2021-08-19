76ac107da13440f4b41f56d3cfc804cdf0bd26bc
Dmitry Igrishin <dmitigr@gmail.com>
2021-08-19 18:48:43 +0300

    driver: refactoring

--- src/driver/board.hpp
+++ src/driver/board.hpp
@@ -19,14 +19,17 @@
 #ifndef PANDA_TIMESWIPE_DRIVER_BOARD
 #define PANDA_TIMESWIPE_DRIVER_BOARD
 
+#include "types_fwd.hpp"
+#include "RaspberryPi/bcmspi.h"
+
 #include <array>
 #include <chrono>
 #include <cstdint>
+#include <iostream>
 #include <list>
+#include <sstream>
 #include <string>
 #include <thread>
-
-#include "types_fwd.hpp"
 
 // PIN NAMES
 static const std::uint8_t DATA0{24};  // BCM 24 - PIN 18
@@ -60,6 +63,274 @@ static const std::uint32_t TCO_POSITION{std::uint32_t{1} << TCO};
 static const std::uint32_t PI_STATUS_POSITION{std::uint32_t{1} << PI_OK};
 static const std::uint32_t FAIL_POSITION{std::uint32_t{1} << FAIL};
 static const std::uint32_t BUTTON_POSITION{std::uint32_t{1} << BUTTON};
+
+class BoardInterface {
+public:
+  inline static bool trace_spi = false;
+
+  ~BoardInterface()
+  {
+    if (instance_) delete instance_;
+  }
+
+  static BoardInterface* get()
+  {
+    if (!instance_) instance_ = new BoardInterface();
+    return instance_;
+  }
+
+  void setMode(int num)
+  {
+    sendSetCommand("Mode", std::to_string(num));
+    std::string answer;
+    //TODO: check answer
+    receiveAnswer(answer);
+  }
+
+  void setEnableADmes(bool value)
+  {
+    sendSetCommand("EnableADmes", value ? "1" : "0" );
+    std::string answer;
+    receiveAnswer(answer);
+  }
+
+  bool getEvents(std::string& ev)
+  {
+    sendEventsCommand();
+    return receiveAnswer(ev);
+  }
+
+  std::string getSetSettings(const std::string& request, std::string& error)
+  {
+    sendSetSettingsCommand(request);
+    std::string answer;
+    if (!receiveAnswer(answer, error)) {
+      error = "read SPI failed";
+    }
+    return answer;
+  }
+
+  std::string getGetSettings(const std::string& request, std::string& error)
+  {
+    sendGetSettingsCommand(request);
+    std::string answer;
+    if (!receiveAnswer(answer, error)) {
+      error = "read SPI failed";
+    }
+    return answer;
+  }
+
+  bool setDAC(bool value)
+  {
+    sendSetCommand("DACsw", value ? "1" : "0");
+    std::string answer;
+    if (!receiveStripAnswer(answer)) return false;
+    return answer == (value ? "1" : "0");
+  }
+
+  // num == 0 -> AOUT3  num == 1 -> AOUT4
+  bool setOUT(uint8_t num, int val)
+  {
+    std::string var = std::string("AOUT") + (num?"4":"3") + ".raw";
+    sendSetCommand(var, std::to_string(val));
+    std::string answer;
+    if (!receiveStripAnswer(answer)) return false;
+    return answer == std::to_string(val);
+  }
+
+  // num == 0 -> PWM1  num == 1 -> PWM2
+  bool startPWM(uint8_t num, uint32_t frequency, uint32_t high, uint32_t low, uint32_t repeats, float duty_cycle);
+
+  // num == 0 -> PWM1  num == 1 -> PWM2
+  bool stopPWM(uint8_t num)
+  {
+    std::string pwm = std::string("PWM") + std::to_string(num+1);
+    /*
+      sendGetCommand(pwm);
+      std::string answer;
+      receiveStripAnswer(answer);
+      if (answer == "0") return false; // Already stopped
+    */
+
+    return sendSetCommandCheck(pwm, 0);
+  }
+
+  // num == 0 -> PWM1  num == 1 -> PWM2
+  bool getPWM(uint8_t num, bool& active, uint32_t& frequency, uint32_t& high, uint32_t& low, uint32_t& repeats, float& duty_cycle);
+
+  std::string makeChCmd(unsigned int num, const char *pSubDomain)
+  {
+
+    char buf[32];
+    std::sprintf(buf, "CH%d.%s", num+1, pSubDomain);
+    return buf;
+  }
+
+  bool setChannelMode(unsigned int num, int nMode)
+  {
+    sendSetCommand(makeChCmd(num, "mode"), std::to_string(nMode));
+    std::string answer;
+    if (!receiveStripAnswer(answer)) return false;
+    return answer == std::to_string(nMode);
+  }
+
+  bool getChannelMode(unsigned int num, int &nMode, std::string& error)
+  {
+    sendGetCommand(makeChCmd(num, "mode"));
+    std::string answer;
+    if (!receiveAnswer(answer, error)) {
+      nMode=0;
+      return false;
+    }
+    nMode=std::stoi(answer);
+    return true;
+
+  }
+
+  bool setChannelGain(unsigned int num, float Gain)
+  {
+    sendSetCommand(makeChCmd(num, "gain"), std::to_string(Gain));
+    std::string answer;
+    if (!receiveStripAnswer(answer)) return false;
+    return true;
+  }
+
+  bool getChannelGain(unsigned int num, float &Gain, std::string& error)
+  {
+    sendGetCommand(makeChCmd(num, "gain"));
+    std::string answer;
+    if (!receiveAnswer(answer, error)) {
+      Gain=0;
+      return false;
+    }
+    Gain=std::stof(answer);
+    return true;
+
+  }
+
+  bool setChannelIEPE(unsigned int num, bool bIEPE)
+  {
+    sendSetCommand(makeChCmd(num, "iepe"), std::to_string(bIEPE));
+    std::string answer;
+    if (!receiveStripAnswer(answer)) return false;
+    return true;
+  }
+
+  bool getChannelIEPE(unsigned int num, bool &bIEPE, std::string& error)
+  {
+    sendGetCommand(makeChCmd(num, "iepe"));
+    std::string answer;
+    if (!receiveAnswer(answer, error)) {
+      bIEPE=0;
+      return false;
+    }
+    bIEPE=std::stoi(answer);
+    return true;
+  }
+
+private:
+  CBcmSPI spi_;
+
+  inline static BoardInterface* instance_;
+
+  BoardInterface()
+    : spi_{CBcmLIB::iSPI::SPI0}
+  {}
+
+  static void stripAnswer(std::string& str) noexcept
+  {
+    if (!str.empty() && str.back() == '\n')
+      str.pop_back();
+  }
+
+  void sendCommand(const std::string& cmd)
+  {
+    CFIFO command;
+    command += cmd;
+    spi_.send(command);
+    if (trace_spi) {
+      std::cerr << "spi: sent: \"" << command << "\"" << std::endl;
+    }
+  }
+
+  bool receiveAnswer(std::string& ans)
+  {
+    CFIFO answer;
+    if (spi_.receive(answer)) {
+      ans = answer;
+      if (trace_spi) {
+        std::cerr << "spi: received: \"" << ans << "\"" << std::endl;
+      }
+      return true;
+    }
+    if (trace_spi) {
+      std::cerr << "spi: receive failed" << std::endl;
+    }
+    return false;
+  }
+
+  bool receiveAnswer(std::string& ans, std::string& error)
+  {
+    auto ret = receiveAnswer(ans);
+    if (ret && !ans.empty() && ans[0]=='!') {
+      error = ans;
+      ans.clear();
+      return false;
+    }
+    return ret;
+  }
+
+  bool receiveStripAnswer(std::string& ans, std::string& error)
+  {
+    if (!receiveAnswer(ans,error)) return false;
+    stripAnswer(ans);
+    return true;
+  }
+
+  bool receiveStripAnswer(std::string& ans)
+  {
+    std::string error;
+    return receiveStripAnswer(ans, error);
+  }
+
+  void sendSetCommand(const std::string& variable, const std::string& value)
+  {
+    sendCommand(variable + "<" + value + "\n");
+  }
+
+  template <class NUMBER>
+  bool sendSetCommandCheck(const std::string& variable, const NUMBER& value) {
+    sendSetCommand(variable, std::to_string(value));
+    // XXX: if sleep disable and trace_spi=false spi_.receive fails sometimes
+    //std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+    std::string answer;
+    receiveStripAnswer(answer);
+    NUMBER num;
+    std::istringstream ss(answer);
+    ss >> num;
+    return num == value;
+  }
+
+  void sendGetCommand(const std::string& variable)
+  {
+    sendCommand(variable + ">\n");
+  }
+
+  void sendEventsCommand()
+  {
+    sendCommand("je>\n");
+  }
+
+  void sendSetSettingsCommand(const std::string& request)
+  {
+    sendCommand("js<" + request + "\n");
+  }
+
+  void sendGetSettingsCommand(const std::string& request)
+  {
+    sendCommand("js>" + request + "\n");
+  }
+};
 
 /**
  * Initializes GPIO pins.
--- src/driver/timeswipe.cpp
+++ src/driver/timeswipe.cpp
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "protocol.hpp"
+#include "board.hpp"
 #include "pidfile.hpp"
 #include "reader.hpp"
 #include "resampler.hpp"
