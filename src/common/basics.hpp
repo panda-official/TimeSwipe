2aad3a6f8a0c86f18e687477456ee2184cb921d9
Dmitry Igrishin <dmitigr@gmail.com>
2021-09-03 15:46:28 +0300

    common: new basics.hpp

---
+++ src/common/basics.hpp
@@ -0,0 +1,23 @@
+// -*- C++ -*-
+/*
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Copyright (c) 2021 PANDA GmbH / Dmitry Igrishin
+*/
+
+#ifndef PANDA_TIMESWIPE_BASICS_HPP
+#define PANDA_TIMESWIPE_BASICS_HPP
+
+namespace panda::timeswipe {
+
+/// Measurement mode.
+enum class Measurement_mode {
+  Voltage,
+  Current
+};
+
+} // namespace panda::timeswipe
+
+#endif  // PANDA_TIMESWIPE_BASICS_HPP
