panda-official/TimeSwipe

commit 80da0e9acee34b5ca503dad14595e59d722fd3a7

firmware: refactor Sam_clock_generator
Rename Factory() to make().
Simplify implementation: don't use std::list for instances.
Add macro PANDA_TIMESWIPE_FIRMWARE_ASSERT().

src/firmware/sam/clock_generator.cpp
@@ -20,39 +20,28 @@
 
 #include <sam.h>
 
-std::list<Sam_clock_generator*> Sam_clock_generator::instances_;
-bool Sam_clock_generator::busy_[12];
-
 Sam_clock_generator::~Sam_clock_generator()
 {
-  instances_.remove(this);
-  busy_[static_cast<int>(id_)] = false;
+  instances_[static_cast<int>(id_)] = nullptr;
 }
 
-//factory:
-std::shared_ptr<Sam_clock_generator> Sam_clock_generator::Factory()
+Sam_clock_generator::Sam_clock_generator(const Id id)
+  : id_{id}
+{}
+
+std::shared_ptr<Sam_clock_generator> Sam_clock_generator::make()
 {
-    for(int i=static_cast<int>(Id::GCLK2); i<=static_cast<int>(Id::GCLK11); i++)
-    {
-        //check if hardware occupied by a first time
-        if (GCLK->GENCTRL[i].bit.GENEN) {
-          busy_[i] = true;
-          continue;
-        }
-
-
-        if(!busy_[i])
-        {
-            busy_[i]=true;
-            Sam_clock_generator *pClk= new Sam_clock_generator; //because of protected ctor
-            pClk->id_ = static_cast<Id>(i);
-            instances_.push_back(pClk);
-            GCLK->GENCTRL[i].bit.SRC=GCLK_GENCTRL_SRC_DFLL; //def source
-            pClk->WaitSync();
-            return std::shared_ptr<Sam_clock_generator>(pClk);
-        }
+  for (int i = static_cast<int>(Id::GCLK2); i <= static_cast<int>(Id::GCLK11); ++i) {
+    if (!instances_[i]) {
+      std::shared_ptr<Sam_clock_generator> instance{new
+        Sam_clock_generator{static_cast<Id>(i)}};
+      GCLK->GENCTRL[i].bit.SRC = GCLK_GENCTRL_SRC_DFLL; // def source
+      instance->WaitSync();
+      instances_[i] = instance.get();
+      return instance;
     }
-    return nullptr;
+  }
+  return nullptr;
 }
 
 void Sam_clock_generator::WaitSync()

CMakeLists.txt
@@ -319,8 +319,8 @@ if(PANDA_TIMESWIPE_FIRMWARE)
     )
 
   # Our stuff
-  set(firmware_src PWM.h Storage.h adcdac.hpp button.h cmd.h frm_stream.h
-    json.hpp os.h pin.hpp std_port.h timer.h)
+  set(firmware_src PWM.h Storage.h adcdac.hpp button.h cmd.h error.hpp
+    frm_stream.h json.hpp os.h pin.hpp std_port.h timer.h)
   list(TRANSFORM firmware_src PREPEND src/firmware/)
   foreach(dep base control json led misc sam)
     list(TRANSFORM ${dep}_src PREPEND src/firmware/${dep}/)

src/firmware/base/DACPWMht.cpp
@@ -5,6 +5,7 @@ file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
 Copyright (c) 2019-2020 Panda Team
 */
 
+#include "../error.hpp"
 #include "DACPWMht.h"
 
 #include <sam.h>
@@ -75,7 +76,8 @@ CDacPWMht::CDacPWMht(PWM nPWM, const std::shared_ptr<Pin> &pDACsw, mode nOpMode)
     //get clock:
     if(!m_pCLK)
     {
-        m_pCLK=Sam_clock_generator::Factory();
+        m_pCLK = Sam_clock_generator::make();
+        PANDA_TIMESWIPE_FIRMWARE_ASSERT(m_pCLK);
         m_pCLK->Enable(true); //???
     }
     CSamTC::ConnectGCLK(m_pCLK->id());

src/firmware/base/PINPWM.cpp
@@ -5,7 +5,7 @@ file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
 Copyright (c) 2019-2020 Panda Team
 */
 
-
+#include "../error.hpp"
 #include "PINPWM.h"
 
 #include <sam.h>
@@ -22,7 +22,8 @@ CPinPWM::CPinPWM(Sam_pin::Group nGroup, Sam_pin::Number nPin) : CSamTC(typeSamTC
     CSamTC::EnableAPBbus(true);
     CSamTC::EnableAPBbus(static_cast<typeSamTC>(static_cast<int>(m_nTC)+1), true);
 
-    m_pCLK=Sam_clock_generator::Factory();
+    m_pCLK=Sam_clock_generator::make();
+    PANDA_TIMESWIPE_FIRMWARE_ASSERT(m_pCLK);
     CSamTC::ConnectGCLK(m_pCLK->id());
     m_pCLK->Enable(true);
 

src/firmware/error.hpp
@@ -1,0 +1,42 @@
+// -*- C++ -*-
+
+// PANDA Timeswipe Project
+// Copyright (C) 2021  PANDA GmbH / Dmitry Igrishin
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef PANDA_TIMESWIPE_FIRMWARE_ERROR_HPP
+#define PANDA_TIMESWIPE_FIRMWARE_ERROR_HPP
+
+#include <exception> // std::terminate
+
+// -----------------------------------------------------------------------------
+// Macros
+// -----------------------------------------------------------------------------
+
+/**
+ * @brief Checks the assertion `a`.
+ *
+ * @details Always active regardless of `NDEBUG`.
+ *
+ * @par Effects
+ * Terminates the process if `!a`.
+ */
+#define PANDA_TIMESWIPE_FIRMWARE_ASSERT(a) do { \
+    if (!(a)) {                                 \
+      std::terminate();                         \
+    }                                           \
+  } while (false)
+
+#endif  // PANDA_TIMESWIPE_FIRMWARE_ERROR_HPP

src/firmware/sam/SamADCcntr.cpp
@@ -5,6 +5,7 @@ file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
 Copyright (c) 2019 Panda Team
 */
 
+#include "../error.hpp"
 #include "SamADCcntr.h"
 #include "NVMpage.h"
 
@@ -171,7 +172,8 @@ CSamADCcntr::CSamADCcntr(typeSamADC nADC)
     //-------------------------------------------------------------------
 
     //----------------------connect default gen--------------------------
-    m_pCLK=Sam_clock_generator::Factory();
+    m_pCLK = Sam_clock_generator::make();
+    PANDA_TIMESWIPE_FIRMWARE_ASSERT(m_pCLK);
 
     int pchind=typeSamADC::Adc0==m_nADC ? GCLK_ADC0:GCLK_ADC1;
     GCLK->PCHCTRL[pchind].bit.GEN=static_cast<uint32_t>(m_pCLK->id());

src/firmware/sam/SamDACcntr.cpp
@@ -7,8 +7,9 @@ Copyright (c) 2019 Panda Team
 
 //SAM DAC controller class impl:
 
-#include "SamDACcntr.h"
+#include "../error.hpp"
 #include "../os.h"
+#include "SamDACcntr.h"
 
 #include <sam.h>
 
@@ -68,7 +69,8 @@ void CSamDACcntr::common_init() //common settings for both dacs
     //----------------------------------------------------------------
 
     //-----------------------connect default generator----------------
-    m_pCLK=Sam_clock_generator::Factory();
+    m_pCLK=Sam_clock_generator::make();
+    PANDA_TIMESWIPE_FIRMWARE_ASSERT(m_pCLK);
 
     //enable gclk: set to Current Control default: CC100K GCLK_DAC ≤ 1.2MHz (100kSPS) 48MHz/64=750KHz
     GCLK->PCHCTRL[GCLK_DAC].bit.GEN=static_cast<uint32_t>(m_pCLK->id());

src/firmware/sam/SamI2CeepromMaster.cpp
@@ -5,9 +5,9 @@ file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.html
 Copyright (c) 2019 Panda Team
 */
 
-
-#include "SamI2CeepromMaster.h"
+#include "../error.hpp"
 #include "../os.h"
+#include "SamI2CeepromMaster.h"
 
 #include <sam.h>
 
@@ -24,7 +24,8 @@ CSamI2CeepromMaster::CSamI2CeepromMaster()
     SetWriteProtection(true);
 
     enable_internal_bus(true);
-    m_pCLK=Sam_clock_generator::Factory();
+    m_pCLK=Sam_clock_generator::make();
+    PANDA_TIMESWIPE_FIRMWARE_ASSERT(m_pCLK);
 
     //connect:
     connect_clock_generator(m_pCLK->id());

commit 6169e9daae4abf2c54b505393e107436ec8c4a35

firmware: fix the regression
Regression was introduced by 80da0e9acee34b5ca503dad14595e59d722fd3a7.

src/firmware/sam/clock_generator.cpp
@@ -34,6 +34,10 @@ Sam_clock_generator::Sam_clock_generator(const Id id)
 std::shared_ptr<Sam_clock_generator> Sam_clock_generator::make()
 {
   for (int i = static_cast<int>(Id::GCLK2); i <= static_cast<int>(Id::GCLK11); ++i) {
+    // The generator may be enabled directly! (For example, sys_clock_init().)
+    if (GCLK->GENCTRL[i].bit.GENEN) continue;
+
+    // Create and register the instance at the free slot.
     if (!instances_[i]) {
       std::shared_ptr<Sam_clock_generator> instance{new
         Sam_clock_generator{static_cast<Id>(i)}};